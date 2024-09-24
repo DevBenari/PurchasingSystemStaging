830fb0917ec856d32b5450b0cbb4e20f03de772e
blackand18 <119662487+blackand18@users.noreply.github.com>
2024-09-24 16:40:26 +0700

    Update Profile 24092024

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -114,8 +114,86 @@ namespace PurchasingSystemApps.Controllers
             return View();
         }
 
-        public IActionResult Privacy()
+        public IActionResult MyProfile()
         {
+            ViewBag.Active = "Dashboard";
+            var countUser = _applicationDbContext.UserActives.GroupBy(u => u.UserActiveId).Select(y => new
+            {
+                UserActiveId = y.Key,
+                CountOfUsers = y.Count()
+            }).ToList();
+            ViewBag.CountUser = countUser.Count;
+
+            var countSupplier = _applicationDbContext.Suppliers.GroupBy(u => u.SupplierId).Select(y => new
+            {
+                SupplierId = y.Key,
+                CountOfSuppliers = y.Count()
+            }).ToList();
+            ViewBag.CountSupplier = countSupplier.Count;
+
+            var countProduct = _applicationDbContext.Products.GroupBy(u => u.ProductId).Select(y => new
+            {
+                ProductId = y.Key,
+                CountOfProducts = y.Count()
+            }).ToList();
+            ViewBag.CountProduct = countProduct.Count;
+
+            var countPurchaseRequest = _applicationDbContext.PurchaseRequests.GroupBy(u => u.PurchaseRequestId).Select(y => new
+            {
+                PurchaseRequestId = y.Key,
+                CountOfPurchaseRequests = y.Count()
+            }).ToList();
+            ViewBag.CountPurchaseRequest = countPurchaseRequest.Count;
+
+            var countPurchaseOrder = _applicationDbContext.PurchaseOrders.GroupBy(u => u.PurchaseOrderId).Select(y => new
+            {
+                PurchaseOrderId = y.Key,
+                CountOfPurchaseOrders = y.Count()
+            }).ToList();
+            ViewBag.CountPurchaseOrder = countPurchaseOrder.Count;
+
+            var countReceiveOrder = _applicationDbContext.ReceiveOrders.GroupBy(u => u.ReceiveOrderId).Select(y => new
+            {
+                ReceiveOrderId = y.Key,
+                CountOfReceiveOrders = y.Count()
+            }).ToList();
+            ViewBag.CountReceiveOrder = countReceiveOrder.Count;
+
+            var checkUserLogin = _userActiveRepository.GetAllUserLogin().Where(u => u.UserName == User.Identity.Name).FirstOrDefault();
+            var user = _userActiveRepository.GetAllUser().Where(u => u.FullName == checkUserLogin.NamaUser).FirstOrDefault();
+
+            if (user != null)
+            {
+                UserActiveViewModel viewModel = new UserActiveViewModel
+                {
+                    UserActiveId = user.UserActiveId,
+                    UserActiveCode = user.UserActiveCode,
+                    FullName = user.FullName,
+                    IdentityNumber = user.IdentityNumber,
+                    DepartmentId = user.DepartmentId,
+                    Department = user.Department.DepartmentName,
+                    PositionId = user.PositionId,
+                    Position = user.Position.PositionName,
+                    PlaceOfBirth = user.PlaceOfBirth,
+                    DateOfBirth = user.DateOfBirth,
+                    Gender = user.Gender,
+                    Address = user.Address,
+                    Handphone = user.Handphone,
+                    Email = user.Email,
+                    UserPhotoPath = user.Foto
+                };
+                return View(viewModel);
+            }
+            else if (user == null && checkUserLogin.NamaUser == "SuperAdmin")
+            {
+                UserActiveViewModel viewModel = new UserActiveViewModel
+                {
+                    UserActiveCode = checkUserLogin.KodeUser,
+                    FullName = checkUserLogin.NamaUser,
+                    Email = checkUserLogin.Email
+                };
+                return View(viewModel);
+            }
             return View();
         }
 
