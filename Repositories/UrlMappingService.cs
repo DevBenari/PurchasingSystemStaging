7de1cfc99816704db94c4721ad6287d9ff104f95
andryzain <dev.benarisolusindo@gmail.com>
2024-11-19 06:40:20 +0700

    FIX Enkripsi URL Pendek

---
+++ Repositories/UrlMappingService.cs
@@ -0,0 +1,8 @@
+﻿namespace PurchasingSystemStaging.Repositories
+{
+    public class UrlMappingService
+    {
+        // In-memory mapping for demo purposes (use a distributed cache or database in production)
+        public Dictionary<string, string> InMemoryMapping { get; } = new();
+    }
+}
