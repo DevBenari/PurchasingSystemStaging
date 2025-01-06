fa95222c04c5775a0170cb9244a6ea3114e2db6d
DevBenari <dev.benarisolusindo@gmail.com>
2025-01-06 14:34:32 +0700

    Update dari Server DateTimeOffset ApplicationUser

---
+++ Migrations/20250106023728_updateLastActivity.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace PurchasingSystemStaging.Migrations
+{
+    public partial class updateLastActivity : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<DateTimeOffset>(
+                name: "LastActivityTime",
+                table: "AspNetUsers",
+                type: "datetimeoffset",
+                nullable: true,
+                oldClrType: typeof(DateTime),
+                oldType: "datetime2",
+                oldNullable: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<DateTime>(
+                name: "LastActivityTime",
+                table: "AspNetUsers",
+                type: "datetime2",
+                nullable: true,
+                oldClrType: typeof(DateTimeOffset),
+                oldType: "datetimeoffset",
+                oldNullable: true);
+        }
+    }
+}
